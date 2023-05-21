18ac210b049b5d826852b52b9f79aab724476380
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-05-21 15:09:18 +0000

    Conversions of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,24 @@
-let score = "33";
-
-console.log(typeof score);
-console.log(typeof (score));
+let score = "33abc";
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "suraj";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "suraj" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
