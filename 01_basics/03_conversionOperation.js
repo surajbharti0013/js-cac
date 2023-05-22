d86b19f16138a0260b573df0166621b6d540e85a
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-05-22 07:04:28 +0000

    Conversions of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,4 +21,33 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// -------------------Operations -----------------------
+
+let value = 3;
+let negValue = -value;
+// console.log(typeof negValue);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " suraj";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+// console.log(((3 + 4) * 5) % 3);
+
+console.log(+true);
