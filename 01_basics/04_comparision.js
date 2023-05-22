118ea78a2f96c20847706d5ba984a6b24858d9e6
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-05-22 15:50:16 +0000

    Comparision operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1); //true
+// console.log(2 >= 1); //true
+// console.log(2 < 1); //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// try not to write code like line 10 to line 16
+
+// ===
+
+// console.log("2" == 2);//true Because it compares 2 and 2.
+// console.log("2" === 2);//false Because it compares both value and its datatype.
