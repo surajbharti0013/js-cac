c905d31113e69d26c3e809a790262575cbc7761a
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-05-24 13:48:32 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "suraj";
+const repoCount = 50;
+console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name}, and my repo count is ${repoCount}`);
+
+const gameName = new String('surajsb');
+console.log(gameName[0]);// This tells on 0 index what text is it.
+console.log(gameName.__proto__);// This tells us is it object etc.
+console.log(gameName.length);// This tells us length of text/string.
+console.log(gameName.toUpperCase());// Convert full text/string to upper case.
