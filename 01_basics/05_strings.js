140649330461dc2c268f031bf5d55642958471df
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-05-24 13:52:59 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,3 +8,5 @@ console.log(gameName[0]);// This tells on 0 index what text is it.
 console.log(gameName.__proto__);// This tells us is it object etc.
 console.log(gameName.length);// This tells us length of text/string.
 console.log(gameName.toUpperCase());// Convert full text/string to upper case.
+console.log(gameName.charAt(2));// This tells on what index what text/character is it.
+console.log(gameName.indexOf('s'));// This tells on what text/character is on what index.
