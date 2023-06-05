faf67cb9f5fc18104a99a6647245b02ba810cfbc
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-06-05 14:47:31 +0000

    DatesInJS

---
+++ 01_basics/07_DatesInJS.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString()); // The toString() method returns a date object as a string. Current day,date and time in gmt+05:30  in 24 hours format
+// console.log(myDate.toDateString()); // The myDate.toDateString() method returns a date object as a string. Current day,date.
+// console.log(myDate.toISOString()); // toISOString() returns a date as string, using the ISO standard:
+// console.log(myDate.toJSON()); // toJSON() returns a date as a string, using JSON date formatting:
+
+// console.log(myDate.toLocaleDateString()); // toLocaleDateString() returns the date (not the time) of a date, as a string, using locale conventions:
+// console.log(myDate.toLocaleString()); // toLocaleString() returns a date as a string, using locale settings:
+// console.log(myDate.toLocaleTimeString()); // toLocaleTimeString() returns the time portion of a date, as a string, using locale conventions:
