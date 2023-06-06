3b6eb5fa3577ddfb74577b7312fe3ef548bca8b7
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-06-06 18:41:54 +0000

    arrays part1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -29,11 +29,10 @@ const myArr2 = new Array(1, 2, 3, 4);
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3);
-
+const myn1 = myArr.slice(1, 3); // slice() show only selected/defined part.
 console.log(myn1);
-console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3);
+console.log("B ", myArr);
+const myn2 = myArr.splice(1, 3); // splice() don't show selected/defined part.
 console.log("C ", myArr);
 console.log(myn2);
