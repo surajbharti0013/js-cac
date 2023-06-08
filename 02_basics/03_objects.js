1708a2650ee4b835cd0b9e19cbf7433b3667253b
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-06-08 15:33:49 +0000

    objects part1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,46 @@
+// singleton - Singleton is a design pattern that tells us that we can create only one instance.
+// Object.create
+
+// When we declare as a literal that time it doesnt create singleton.
+
+// Whem we create via constructor then it creates singleton.
+
+// object literals
+
+const mySym = Symbol("key1");
+
+const jsUser = {
+  //keys: value;
+  name: "Suraj",
+  "full name": "Suraj Bharti",
+  [mySym]: "mykey1",
+  age: 30,
+  location: "Mumbai",
+  email: "suraj@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+// Two type to access value
+// console.log(jsUser.email); // Ok Option
+// console.log(jsUser["email"]); //Better option
+
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+
+jsUser.email = "suraj@chatgpt.com"; // to change value of some key.
+// console.log(jsUser.email);
+
+// Object.freeze(jsUser);
+jsUser.email = "suraj@microsoft.com";
+// console.log(jsUser.email);
+
+jsUser.greeting = function () {
+  console.log("Hello js user");
+};
+jsUser.greetingTwo = function () {
+  console.log(`Hello js user, ${this.name} `);
+};
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
