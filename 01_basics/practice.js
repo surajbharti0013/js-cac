adfd7b68ead0965688cad4c2741f89d39791c3ca
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-06-13 05:13:20 +0000

    practice

--- 01_basics/practice.js
+++
@@ -1,9 +0,0 @@
-const newName = new String('abc def');
-console.log(newName);
-
-console.log(newName.anchor());
-console.log(newName.at());// first character of string/sentence
-console.log(newName.big());// first character of string/sentence
-console.log(newName.blink());// first character of string/sentence
-
-
