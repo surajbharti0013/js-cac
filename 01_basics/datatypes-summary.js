a9bf75c39798dda43b492a2c5305a9ab53a721ff
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-06-13 05:19:15 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,73 +1,67 @@
-// Javascript is Dynamically typed language because we dont have to specify variables datatype (int, string, boolean etc).
+/* How data is kept in memory and how it is accessed on that  we have 2 type of data types.
 
-// Datatypes are categorized in 2 types, How data is kept in memory and how it is accessed Primitive and Non-Primitive(referrence type).
+    a.Primitive data types (Call by value) Stack Memory - String, Number, Boolean, null, undefined, Symbol, BigInt.(What ever changes you do in it you do it in copy).
+    
+    b.Non-Primitive data types (Reference type) Heap Memory - Array, Objects, Functions.(What ever changes you do in it you do it in Reference copy of original value).
+*/
 
-// Primitive 7 types:- string(text in single or double quotes), number (1, 2, 3...), boolean(true or false), null(empty not zero), undefined(we have defined variable but not yet decided what to keep in it), Symbol, BigInt(Maninly all values are covered in number but some values are long in decimal character that why we use BigInt to store that value.)
+// Javascript is Dynamically typed language because we don't have to specify variables datatype (int, string, boolean etc).
 
-const name = "Suraj";//String
-//console.log(typeof name); // typeof string
+const score = "100";
+// console.log(typeof score); // String
 
-const yourNumber = 123456; //Number
-//console.log(typeof yourNumber); // typeof number
+const scoreResult = 100;
+// console.log(typeof scoreResult); // Number
 
-const isBoolean = true; //boolean(true or false)
-//console.log(typeof isBoolean); // typeof boolean
+const isLoggedIn = false;
+// console.log(typeof isLoggedIn); // Boolean
 
-const outsideTemp = null;//(null temp not zero)
-//console.log(typeof outsideTemp); // typeof object
+const outsideTemp = null;
+// console.log(typeof outsideTemp); // null/object
 
-const userEmail = undefined;//(have created the variable but not defined it yet)
-//console.log(typeof userEmail); // typeof undefined
+let userEmail;
+// console.log(typeof userEmail); // Undefined
 
-const Id = Symbol('123');
-const anotherId = Symbol('123');
-//console.log(typeof anotherId); // typeof symbol
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
-//console.log(Id === anotherId);
+// console.log(typeof id); // symbol
+// console.log(id === anotherId); // false
 
-const bigNumber = 345678945612332581n;
-//console.log(typeof bigNumber); // typeof BigInt
+const bigNumber = 3456543576654356754n; // bigInt
+// console.log(typeof bigNumber);
 
-// Non-Premitive type(Reference type):- Array, Objects and Functions
+const heroes = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+  name: "suraj",
+  age: 30,
+};
 
-const heros = ["shaktiman", "naagraj", "doga"]; //Array
-//console.log(typeof heros); // typeof object
+const myFunction = function () {
+  // console.log("Hello World");
+};
 
-let myObject = {
-    //Thing in curly braces are objects they are usually in key: value paires
-    name:"suraj",
-    age:22,
-} // eg. for objects
+// +++++++++++++++++++++++++++++++++
 
-//console.log(typeof myObject); // typeof object
+// Stack (Primitive), Heap (Non-Primitive)
 
-const myFunction = function() {
-    console.log("Hello World");
-}
-
-//console.log(typeof myFunction); // typeof function
-
-// +++++++++++++++++++++++++ Stack and Heap Memory +++++++++++++++++++++++++++++++++++++++++++++++++
-
-// Stack(Primitive) and Heap(Non-Primitive) Memory
-// Stack(Primitive) - Whenever, we keep any value in stack then we change value the reference data changes.
-// Heap(Non-Primitive) - Whenever, we keep any value in heap then we change value the original data changes.
-let myName = "surajbharti";
+let myName = "surajbharti.com";
 
 let anotherName = myName;
-anotherName = "raj";
+
+anotherName = "suraj";
 
 console.log(myName);
 console.log(anotherName);
 
 let userOne = {
-    email: "user@google.com",
-    upi:"user@ybl"
-}
+  email: "user@google.com",
+  upi: "user@ybl",
+};
 
 let userTwo = userOne;
 
-userTwo.email = "hitesh@google.com";
+userTwo.email = "suraj@google.com";
 
 console.log(userOne.email);
 console.log(userTwo.email);
