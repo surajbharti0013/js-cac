97750398e1196c16a418498881c7ee8b084c25e4
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-06-21 16:04:21 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,48 +1,58 @@
-const user = {
-    username : "suraj",
-    price : 999,
-  
-    welcomeMessage: function() {
-      console.log(`${this.username} , welcome to website`);// This keyword is use when you are talking about current context
-      // console.log(this);
-    }
-  }
-  
-  // user.welcomeMessage()
-  // user.username = "sam"
-  // user.welcomeMessage()
-  
-  // console.log(this);
-  
-  // function chai(){
-  //   let username = "suraj"
-  //   console.log(this.username);
-  // }
-  // chai()
-  
-  // const chai = function() {
-  //   let username = "suraj"
-  //   console.log(this.username);
-  // }
-  
-  // chai()
-  
-  // const chai = () => {
-  //   let username = "suraj"
-  //   console.log(this);
-  // }
-  
-  // chai()
-  
-  // const addTwo = (num1, num2) => { //Explecitly arrow function.
-  //   return num1 + num2
-  // }
-  
-  // console.log(addTwo(3, 4))
-  
-  // const addTwo = (num1, num2) => num1 + num2 // Implecit arrow function
-  
-  // console.log(addTwo(3, 4))
-  
-  // const addTwo = (num1, num2) => ({username: "hitesh"})
-  // console.log(addTwo(3, 4))  
+// const user = {
+//   username : "suraj",
+//   price : 999,
+
+//   welcomeMessage: function() {
+//     console.log(`${this.username} , welcome to website`);// This keyword is use when you are talking about current context
+     // console.log(this);
+//   }
+// }
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//   console.log(this)
+// }
+
+// chai()
+
+// function chai(){
+//   let username = "suraj"
+//   console.log(this.username);
+// }
+// chai()
+
+// const chai = function() {
+//   let username = "suraj"
+//   console.log(this.username);
+// }
+
+// chai()
+
+// const chai = () => {
+//   let username = "suraj"
+//   console.log(this);
+// }
+
+// chai()
+
+// const chai = () => {
+//   let username = "suraj"
+//   console.log(this.username);
+// }
+// chai()
+
+// const addTwo = (num1, num2) => { //Explecitly arrow function.
+//   return num1 + num2
+// }
+// console.log(addTwo(3, 4))
+
+// const addTwo = (num1, num2) => num1 + num2 // Implecit return arrow function
+// console.log(addTwo(3, 4))
+
+const addTwo = (num1, num2) => ({username: "hitesh", password: "123"})
+console.log(addTwo(3, 4))
