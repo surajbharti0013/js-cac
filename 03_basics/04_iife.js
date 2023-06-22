3ea1ad2a689ac2adecec8b90c91cc89a5dac069c
Suraj Bharti <134131358+surajbharti0013@users.noreply.github.com>
2023-06-22 09:04:16 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,28 @@
+// Immediately Invoked Function Expressions (IIFE)
+// IIFE is used because to clear pollution caused by global variable. IIFE functions are functions which are invoked immediately.
+
+// function chai(){
+//   console.log(`DB CONNECTED`);
+// }
+// chai()
+
+// Both way to write IIFE is correct
+
+// (function chai(){
+//   console.log(`DB CONNECTED`);
+// })
+// ();
+
+// ************ IIFE using arrow function
+
+// ( () => {
+// Also known as named IIFE
+//   console.log(`DB CONNECTED`);
+// } )()
+
+// ************ IIFE using arrow function with variable
+
+( (name) => {
+    // Also known as named without name IIFE
+    console.log(`DB CONNECTED ${name}`);
+  } )('suraj')
